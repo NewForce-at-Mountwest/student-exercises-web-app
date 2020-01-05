e03f3ac4b4eb776c8732ffe13522191e5664403f
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-01-05 10:58:43 -0500

    added dispaly name to student model

--- StudentExercisesWebApp/Models/Student.cs
+++ StudentExercisesWebApp/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace StudentExercisesWebApp.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
